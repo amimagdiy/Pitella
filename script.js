e568ba43d08716c49da0fcba4458b27d3257f971
Magdy <khaskia.03@gmail.com>
2025-12-19 22:22:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const toggleBtn = document.getElementById("langToggle");
+const htmlRoot = document.getElementById("htmlRoot");
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".navbar ul");
+
+let currentLang = localStorage.getItem("lang") || "ar";
+applyLanguage(currentLang);
+
+toggleBtn.addEventListener("click", () => {
+  currentLang = currentLang === "en" ? "ar" : "en";
+  localStorage.setItem("lang", currentLang);
+  applyLanguage(currentLang);
+});
+
+function applyLanguage(lang){
+  document.querySelectorAll("[data-en]").forEach(el=>{
+    el.textContent = el.getAttribute(`data-${lang}`);
+  });
+
+  if(lang==="ar"){
+    htmlRoot.setAttribute("lang","ar");
+    htmlRoot.setAttribute("dir","rtl");
+    toggleBtn.textContent="EN";
+  }else {
+    htmlRoot.setAttribute("lang","en");
+    htmlRoot.setAttribute("dir","ltr");
+    toggleBtn.textContent="AR";
+  }
+}
+
+// Hamburger Menu
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navList.classList.toggle("active");
+});
