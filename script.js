2348e43bc341d7cace70b56cd1139cc44dd76429
Magdy <khaskia.03@gmail.com>
2025-12-19 19:41:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const toggleBtn = document.getElementById("langToggle");
 const htmlRoot = document.getElementById("htmlRoot");
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".navbar ul");
 
 let currentLang = localStorage.getItem("lang") || "ar";
 applyLanguage(currentLang);
@@ -18,10 +20,15 @@ function applyLanguage(lang){
   if(lang==="ar"){
     htmlRoot.setAttribute("lang","ar");
     htmlRoot.setAttribute("dir","rtl");
-    toggleBtn.textContent="EN";
+    toggleBtn.querySelector(".lang-text").textContent = "EN";
   }else{
     htmlRoot.setAttribute("lang","en");
     htmlRoot.setAttribute("dir","ltr");
-    toggleBtn.textContent="AR";
+    toggleBtn.querySelector(".lang-text").textContent = "AR";
   }
 }
+
+// Hamburger Menu
+hamburger.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
