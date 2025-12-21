bb5e5086499e21c12f1752c0ee62fc0bccf5c321
Magdy <khaskia.03@gmail.com>
2025-12-21 14:46:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,10 @@ function applyLanguage(lang) {
 window.addEventListener("scroll", () => {
   navbar.classList.toggle("scrolled", window.scrollY > 50);
 });
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+
+hamburger.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+
